6e9bf0d66c5c823cb8cf245e7e9bffce61ce27cd
koyama_jun <koyama_jun@8f9aa51a-596e-4e8e-8651-d15d81e860a8>
2012-09-20 06:26:37 +0000

    SL6.1に移行したのでsort -Vに変更
    git-svn-id: http://172.17.127.150/svn/repos/petpic/trunk/src/infra@1617 8f9aa51a-596e-4e8e-8651-d15d81e860a8

--- capistrano/config/deploy/production.rb
+++ capistrano/config/deploy/production.rb
@@ -8,7 +8,7 @@ role :web, "10.5.5.113" # ap02
 
 # 最新のTagをデプロイ
 set :repository_root,  "http://172.17.127.150/svn/repos/petpic/tags/server"
-set :tag, %x[ svn list #{repository_root} | sort | tail -n 1 ].gsub(/(\r\n|\r|\n|\/)/, "").strip
+set :tag, %x[ svn list #{repository_root} | sort -V | tail -n 1 ].gsub(/(\r\n|\r|\n|\/)/, "").strip
 set :repository, "#{repository_root}/#{tag}"
 
 #set :repository,  "http://172.17.127.150/svn/repos/petpic/trunk/src/server"
